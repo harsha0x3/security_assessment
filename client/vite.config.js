776fc0bc6f439b4f0bbd7655ce47cc4c9481a519
harsha0x3 <cgharshavardhan05@gmail.com>
2025-09-05 15:53:55 +0530

    prod_spec_changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["lightningcss"],
-  },
+  server:{
+port:8057,
+host: '0.0.0.0'
+}
 });
